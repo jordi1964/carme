dc773cbb0e57d176b68d15f483b287df900559e8
jordi1964 <41989049+jordi1964@users.noreply.github.com>
2019-02-14 11:02:59 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 
 
 /** Script detecció rotació **/
-/* Copiat de https://davidwalsh.name/orientation-change  */
+/* Copiat de https://davidwalsh.name/orientation-change */
 
 // Find matches
 var mql = window.matchMedia("(orientation: portrait)");
@@ -11,18 +11,44 @@ var mql = window.matchMedia("(orientation: portrait)");
 // If there are matches, we're in portrait
 if(mql.matches) {
  // Portrait orientation
- document.getElementById('nono').style.display = "block";
+ function() {
+   document.getElementById('nono').style.display = "block";
+ }
 } else {
  // Landscape orientation
- document.getElementById('nicas').style.display = "block";
+ function() {
+   document.getElementById('nicas').style.display = "block";
+ }
 }
 
 // Add a media query change listener
 mql.addListener(function(m) {
  if(m.matches) {
    // Changed to portrait
+   function() {
+     document.getElementById('brrt').style.display = "block";
+   }
  }
  else {
    // Changed to landscape
  }
 });
+
+
+
+/* Script copiat de w3schools -> https://www.w3schools.com/jsref/met_win_matchmedia.asp
+
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    document.getElementById('nono').style.display = "block";
+  } else {
+    document.getElementById('nicas').style.display = "block";
+  }
+}
+
+var x = window.matchMedia("(orientation: portrait)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
+*/
